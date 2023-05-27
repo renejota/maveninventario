158aa4f272e727d13eb7bf20f4a18413bd5f5bce
Carlos Kamisato <carloskamisato.ktd@orakle.cl>
2023-05-26 20:08:54 -0400

    creacion de proyecto

---
+++ src/main/java/com/kibernumacademy/miapp/App.java
@@ -0,0 +1,8 @@
+package com.kibernumacademy.miapp;
+
+public class App {
+	
+	public static void main(String[] args ) {
+		System.out.println("Hola a todos, esta es la actividd del Modulo2 Clase4");
+	}
+}
